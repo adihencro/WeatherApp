67d23bdc594668c785d99aae0b70b0f90dc9641e
adihencro <adihencro1412@gmail.com>
2024-04-01 14:04:25 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Search from './components/search/search';
+import CurrentWeather from './components/current-weather/current-weather';
+import { WEATHER_API_KEY, WEATHER_API_URL } from './api';
+import { useState } from 'react';
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+    const [lat, lon] = searchData.value.split(" ");
+
+    const CurrentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+
+    Promise.all([CurrentWeatherFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        setCurrentWeather({city: searchData.label, ...weatherResponse});
+      })
+      .catch((err) => console.log(err));
+  }
+  console.log(currentWeather);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange}/>
+      {currentWeather && <CurrentWeather data={currentWeather} />} 
     </div>
   );
 }
+
 
 export default App;
---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "81f2cbcdeamsh35d05a24a877693p133380jsn228ac2bef307",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+ export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+ export const WEATHER_API_KEY = "e0696a2d1c54f64b0bfd9957f861a315";
